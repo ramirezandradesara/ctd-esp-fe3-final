a84d60ebc6b130a60f589c28dc108ae3a9dc15ab
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2023-03-27 21:21:59 -0300

    env local

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
-import {CssBaseline, ThemeProvider} from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import LayoutGeneral from "dh-marvel/components/layouts/layout-general";
-import {theme} from "dh-marvel/styles/material-theme";
+import { theme } from "dh-marvel/styles/material-theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <ThemeProvider theme={theme}>
